280204442e7a7821760eb067a3e3e70281cd2d28
gjj-mapa <cashman91431+gjj@gmail.com>
2023-06-21 20:44:25 +0200

    Cleanup

--- src/main/java/com/agentkosticka/shared/SharedMethods.java
+++ src/main/java/com/agentkosticka/shared/SharedMethods.java
@@ -8,6 +8,7 @@ public class SharedMethods {
     public static void sendCustomMessageToClient(String message) {
         PlayerEntity player = MinecraftClient.getInstance().player;
         if(player != null){
+            //looks cool, right?
             player.sendMessage(Text.of("§b[MCCI HITW Helper]§r "+message));
         }
     }
